f1c126d913aecf3c3eedf421eecf114157124d3e
Yamamoto Ren <ren.yamamoto@azone-japan.co.jp>
2023-10-30 00:36:02 +0900

    render対応

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,16 +1,28 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const { param, body, validationResult } = require('express-validator');
 const authenticationEnsurer = require('./authentication-ensurer');
 const { v4: uuidv4 } = require('uuid');
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient({ log: [ 'query' ] });
+const prisma = new PrismaClient({ log: ['query'] });
 
 router.get('/new', authenticationEnsurer, (req, res, next) => {
-  res.render('new', { user: req.user });
+  res.render('new', { user: req.user, csrfToken: req.csrfToken() });
 });
 
 router.post('/', authenticationEnsurer, async (req, res, next) => {
+  await body('scheduleName').isString().run(req);
+  await body('candidates').isString().run(req);
+  await body('memo').isString().run(req);
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const err = new Error('入力された情報が不十分または正しくありません。');
+    err.status = 400;
+    return next(err);
+  }
+
   const scheduleId = uuidv4();
   const updatedAt = new Date();
   const schedule = await prisma.schedule.create({
@@ -19,28 +31,37 @@ router.post('/', authenticationEnsurer, async (req, res, next) => {
       scheduleName: req.body.scheduleName.slice(0, 255) || '（名称未設定）',
       memo: req.body.memo,
       createdBy: parseInt(req.user.id),
-      updatedAt: updatedAt
-    }
+      updatedAt: updatedAt,
+    },
   });
   await createCandidatesAndRedirect(parseCandidateNames(req), schedule.scheduleId, res);
 });
 
 router.get('/:scheduleId', authenticationEnsurer, async (req, res, next) => {
+  await param('scheduleId').isUUID('4').run(req);
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const err = new Error('URL の形式が正しくありません。');
+    err.status = 400;
+    return next(err);
+  }
+
   const schedule = await prisma.schedule.findUnique({
     where: { scheduleId: req.params.scheduleId },
     include: {
       user: {
         select: {
           userId: true,
-          username: true
-        }
-      }
-    }
+          username: true,
+        },
+      },
+    },
   });
   if (schedule) {
     const candidates = await prisma.candidate.findMany({
       where: { scheduleId: schedule.scheduleId },
-      orderBy: { candidateId: 'asc' }
+      orderBy: { candidateId: 'asc' },
     });
     // データベースからその予定の全ての出欠を取得する
     const availabilities = await prisma.availability.findMany({
@@ -50,10 +71,10 @@ router.get('/:scheduleId', authenticationEnsurer, async (req, res, next) => {
         user: {
           select: {
             userId: true,
-            username: true
-          }
-        }
-      }
+            username: true,
+          },
+        },
+      },
     });
     // 出欠 MapMap を作成する
     const availabilityMapMap = new Map(); // key: userId, value: Map(key: candidateId, value: availability)
@@ -66,15 +87,15 @@ router.get('/:scheduleId', authenticationEnsurer, async (req, res, next) => {
     // 閲覧ユーザと出欠に紐づくユーザからユーザ Map を作る
     const userMap = new Map(); // key: userId, value: User
     userMap.set(parseInt(req.user.id), {
-        isSelf: true,
-        userId: parseInt(req.user.id),
-        username: req.user.username
+      isSelf: true,
+      userId: parseInt(req.user.id),
+      username: req.user.username,
     });
     availabilities.forEach((a) => {
       userMap.set(a.user.userId, {
         isSelf: parseInt(req.user.id) === a.user.userId, // 閲覧ユーザ自身であるかを示す真偽値
         userId: a.user.userId,
-        username: a.user.username
+        username: a.user.username,
       });
     });
 
@@ -91,9 +112,9 @@ router.get('/:scheduleId', authenticationEnsurer, async (req, res, next) => {
 
     // コメント取得
     const comments = await prisma.comment.findMany({
-      where: { scheduleId: schedule.scheduleId }
+      where: { scheduleId: schedule.scheduleId },
     });
-    const commentMap = new Map();  // key: userId, value: comment
+    const commentMap = new Map(); // key: userId, value: comment
     comments.forEach((comment) => {
       commentMap.set(comment.userId, comment.comment);
     });
@@ -104,7 +125,7 @@ router.get('/:scheduleId', authenticationEnsurer, async (req, res, next) => {
       candidates: candidates,
       users: users,
       availabilityMapMap: availabilityMapMap,
-      commentMap: commentMap
+      commentMap: commentMap,
     });
   } else {
     const err = new Error('指定された予定は見つかりません');
@@ -114,18 +135,29 @@ router.get('/:scheduleId', authenticationEnsurer, async (req, res, next) => {
 });
 
 router.get('/:scheduleId/edit', authenticationEnsurer, async (req, res, next) => {
+  await param('scheduleId').isUUID('4').run(req);
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const err = new Error('URL の形式が正しくありません。');
+    err.status = 400;
+    return next(err);
+  }
+
   const schedule = await prisma.schedule.findUnique({
-    where: { scheduleId: req.params.scheduleId }
+    where: { scheduleId: req.params.scheduleId },
   });
-  if (isMine(req, schedule)) { // 作成者のみが編集フォームを開ける
+  if (isMine(req, schedule)) {
+    // 作成者のみが編集フォームを開ける
     const candidates = await prisma.candidate.findMany({
       where: { scheduleId: schedule.scheduleId },
-      orderBy: { candidateId: 'asc' }
+      orderBy: { candidateId: 'asc' },
     });
     res.render('edit', {
       user: req.user,
       schedule: schedule,
-      candidates: candidates
+      candidates: candidates,
+      csrfToken: req.csrfToken(),
     });
   } else {
     const err = new Error('指定された予定がない、または、予定する権限がありません');
@@ -139,8 +171,20 @@ function isMine(req, schedule) {
 }
 
 router.post('/:scheduleId/update', authenticationEnsurer, async (req, res, next) => {
+  await param('scheduleId').isUUID('4').run(req);
+  await body('scheduleName').isString().run(req);
+  await body('candidates').isString().run(req);
+  await body('memo').isString().run(req);
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const err = new Error('URL の形式が正しくないか、入力された情報が不十分または正しくありません。');
+    err.status = 400;
+    return next(err);
+  }
+
   let schedule = await prisma.schedule.findUnique({
-    where: { scheduleId: req.params.scheduleId }
+    where: { scheduleId: req.params.scheduleId },
   });
   if (isMine(req, schedule)) {
     const updatedAt = new Date();
@@ -149,8 +193,8 @@ router.post('/:scheduleId/update', authenticationEnsurer, async (req, res, next)
       data: {
         scheduleName: req.body.scheduleName.slice(0, 255) || '（名称未設定）',
         memo: req.body.memo,
-        updatedAt: updatedAt
-      }
+        updatedAt: updatedAt,
+      },
     });
     // 候補が追加されているかチェック
     const candidateNames = parseCandidateNames(req);
@@ -167,8 +211,17 @@ router.post('/:scheduleId/update', authenticationEnsurer, async (req, res, next)
 });
 
 router.post('/:scheduleId/delete', authenticationEnsurer, async (req, res, next) => {
+  await param('scheduleId').isUUID('4').run(req);
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const err = new Error('URL の形式が正しくありません。');
+    err.status = 400;
+    return next(err);
+  }
+
   const schedule = await prisma.schedule.findUnique({
-    where: { scheduleId: req.params.scheduleId }
+    where: { scheduleId: req.params.scheduleId },
   });
   if (isMine(req, schedule)) {
     await deleteScheduleAggregate(schedule.scheduleId);
@@ -192,16 +245,19 @@ router.deleteScheduleAggregate = deleteScheduleAggregate;
 async function createCandidatesAndRedirect(candidateNames, scheduleId, res) {
   const candidates = candidateNames.map((c) => ({
     candidateName: c,
-    scheduleId: scheduleId
+    scheduleId: scheduleId,
   }));
   await prisma.candidate.createMany({
-    data: candidates
+    data: candidates,
   });
   res.redirect('/schedules/' + scheduleId);
 }
 
 function parseCandidateNames(req) {
-  return req.body.candidates.split('\n').map((s) => s.trim()).filter((s) => s !== '');
+  return req.body.candidates
+    .split('\n')
+    .map((s) => s.trim())
+    .filter((s) => s !== '');
 }
 
 module.exports = router;
